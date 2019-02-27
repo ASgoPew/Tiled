e5b9d861d96939ead56c2c4d11d1aee1da58e3b0
ASgoPew <35419712+ASgoPew@users.noreply.github.com>
2019-02-27 15:54:27 +0300

    It doesn't work tho

--- Tiled/1d/1dTileProvider.cs
+++ Tiled/1d/1dTileProvider.cs
@@ -13,18 +13,26 @@ namespace Tiled.OneDimension
         public int Width => this._width;
         public int Height => this._height;
 
+        public OneDimensionTileProvider()
+        {
+            data = new StructTile[(Main.maxTilesX + 1) * (Main.maxTilesY + 1)];
+
+            this._width = Main.maxTilesX + 1;
+            this._height = Main.maxTilesY + 1;
+        }
+
+        public OneDimensionTileProvider(int width, int height)
+        {
+            data = new StructTile[(width + 1) * (height + 1)];
+
+            this._width = width + 1;
+            this._height = height + 1;
+        }
+
         public ITile this[int x, int y]
         {
             get
             {
-                if (data == null)
-                {
-                    data = new StructTile[(Main.maxTilesX + 1) * (Main.maxTilesY + 1)];
-
-                    this._width = Main.maxTilesX + 1;
-                    this._height = Main.maxTilesY + 1;
-                }
-
                 return new OneDimensionTileReference(data, x, y);
             }
 
